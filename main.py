05bace5b75e651fc35fbe4b454f661bdc0c2674f
othmanekahtal <othmanekahtal@gmail.com>
2021-08-05 00:04:50 +0100

    add more feature to scaping more data!

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ urls = [
     # 'https://www.allsides.com/media-bias/media-bias-ratings?page=1',
     # 'https://www.allsides.com/media-bias/media-bias-ratings?page=2'
 ]
+data = []
 for i, url in enumerate(urls):
+    # print(i,url)
     r = fetch(url)
-    # parsing data :
+    # # parsing data :
     soup = BeautifulSoup(r['content'], 'html.parser')
     # print(soup.prettify())
+    #
+    # # So to get each row, we just select all <tr> inside <tbody>:
 
-    # So to get each row, we just select all <tr> inside <tbody>:
-    data = []
     rows = soup.select('tbody tr')
     for row in rows:
         d = dict()
@@ -29,16 +31,18 @@ for i, url in enumerate(urls):
         d['disagree'] = int(row.select_one('.disagree').text)
         d['agree_ratio'] = d['agree'] / d['disagree']
         d['agreeance_text'] = get_agreeance(d['agree_ratio'])
-        #getting link of the web site
-        for d in tqdm_notebook(data):
-            r = fetch(d['allsides_page'])
-            soup = BeautifulSoup(r['content'], 'html.parser')
-            try:
-                website = soup.select_one('.www')['href']
-                d['website'] = website
-            except TypeError:
-                pass
-            sleep(10)
+    #     #getting link of the web site
+        link = fetch(d['allsides_page'])
+        soup = BeautifulSoup(link['content'], 'html.parser')
+        # print('fetching link page',soup)
+        try:
+            website = soup.select_one('body > div.full-news-source > div > div > div.span4 > div > a')['href']
+            print(website)
+            d['website'] = website
+        except TypeError:
+            pass
+        sleep(10)
+        print(d)
         data.append(d)
     with open('content/allsides' + str(i) + '.json', 'w') as f:
         json.dump(data, f)
